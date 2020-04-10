6ffc1bbebbf99cd3e69b5680419de372fc5ce189
Himanshu Chauhan <himanshuchauhan981@gmail.com>
2020-04-10 12:08:12 +0530

    removed frontend structure

--- app/src/reducers/auth.reducer.js
+++
@@ -1,3 +0,0 @@
-export default (state = {}, action) =>{
-   return state
-}
--- app/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-
-import authReducers from './auth.reducer'
-
-const reducers = {
-   authReducers
-}
-
-export default combineReducers(reducers)
