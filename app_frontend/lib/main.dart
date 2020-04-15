f3eb7251e2082b2c4f9937974a4261e411232f06
Himanshu Chauhan <himanshuchauhan981@gmail.com>
2020-04-15 17:29:30 +0530

    added home screen

--- app_frontend/lib/main.dart
+++ app_frontend/lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+
 import 'package:app_frontend/pages/signup.dart';
 import 'package:app_frontend/pages/login.dart';
 import 'package:app_frontend/pages/start.dart';
+import 'package:app_frontend/pages/home.dart';
 
 void main() => runApp(MaterialApp(
   initialRoute: '/',
   routes: {
     '/': (context) => Start(),
     '/login': (context) => Login(),
-    '/signup': (context) => Signup()
+    '/signup': (context) => Signup(),
+    '/home': (context) => Home()
   },
 ));
