b09d46f0dd1dc98323a3152f17608d851d86dd8f
Himanshu Chauhan <himanshuchauhan981@gmail.com>
2020-04-16 19:13:09 +0530

    Fixes #13

---
+++ app_frontend/lib/components/sidebar.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+class Sidebar extends StatefulWidget {
+  @override
+  _SidebarState createState() => _SidebarState();
+}
+
+class _SidebarState extends State<Sidebar> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          ListView(
+            shrinkWrap: true,
+            children: <Widget>[
+              ListTile(
+                leading: Icon(Icons.home),
+                title:Text(
+                  'HOME',
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1.0
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.shopping_cart),
+                title: Text(
+                  'SHOP',
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1.0
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.local_mall),
+                title: Text(
+                  'BAG',
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1.0
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.search),
+                title: Text(
+                  'SEARCH',
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1.0
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.local_shipping),
+                title: Text(
+                  'ORDERS',
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1.0
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.favorite_border),
+                title: Text(
+                  'WISHLIST',
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1.0
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text(
+                  'PROFILE',
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1.0
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: new Icon(Icons.exit_to_app),
+                title: Text(
+                  'LOGOUT',
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1.0
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
--- app_frontend/lib/pages/home.dart
+++ app_frontend/lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:app_frontend/components/sidebar.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -6,8 +7,53 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+            'Shop Mart',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 22.0
+          ),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 1.0,
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+            icon: Icon(
+              Icons.menu,
+              size: 35.0,
+              color: Colors.black,
+            ),
+          onPressed: () {
+              if(_scaffoldKey.currentState.isDrawerOpen == false){
+                _scaffoldKey.currentState.openDrawer();
+              }
+              else{
+                _scaffoldKey.currentState.openEndDrawer();
+              }
+          }
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.shopping_basket,
+              size: 35.0,
+              color: Colors.black,
+            ),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Scaffold(
+        key: _scaffoldKey,
+          drawer: new Sidebar()
+      ),
+    );
   }
 }
