0e231e156122ab1dc040f0138f8e518c3dd986c3
Himanshu Chauhan <himanshuchauhan981@gmail.com>
2020-04-22 11:51:15 +0530

    scrolling problem solved in home page

--- app_frontend/lib/components/home/gridItemList.dart
+++ app_frontend/lib/components/home/gridItemList.dart
@@ -26,18 +26,31 @@ class _GridItemListState extends State<GridItemList> {
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return GridView.count(
-      crossAxisCount: 2,
-      children: List.generate(featuredItems.length, (index){
-        return Center(
-          child: Text(
-            'Item $index',
-            style: Theme.of(context).textTheme.headline,
-          ),
-        );
-      })
+  Widget build(BuildContext context){
+    return SliverGrid(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2
+        ),
+        delegate: SliverChildBuilderDelegate((BuildContext context, int index){
+          var item = featuredItems[index];
+          return featuredItemCard(item);
+        },
+            childCount: featuredItems.length
+        )
+    );
+  }
+
+  Widget featuredItemCard(item){
+    return Card(
+      semanticContainer: true,
+      clipBehavior: Clip.antiAliasWithSaveLayer,
+      child: Image.network(
+        item['image'],
+        fit: BoxFit.fill,
+      ),
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0)
+      ),
     );
   }
 }
