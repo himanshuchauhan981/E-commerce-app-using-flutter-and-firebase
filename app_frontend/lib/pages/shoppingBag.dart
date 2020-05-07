59e4a423994a604fff0abbfdc3452b95bd8028ec
Himanshu Chauhan <himanshuchauhan981@gmail.com>
2020-05-07 21:28:19 +0530

    completed bag page

--- app_frontend/lib/pages/shoppingBag.dart
+++ app_frontend/lib/pages/shoppingBag.dart
@@ -1,6 +1,9 @@
 import 'package:app_frontend/components/header.dart';
+import 'package:app_frontend/components/sidebar.dart';
 import 'package:app_frontend/services/orderService.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class ShoppingBag extends StatefulWidget {
   @override
@@ -17,6 +20,12 @@ class _ShoppingBagState extends State<ShoppingBag> {
       bagItemList = bagItems;
     });
   }
+  Size _textSize(String text, TextStyle style) {
+    final TextPainter textPainter = TextPainter(
+        text: TextSpan(text: text, style: style), maxLines: 1, textDirection: TextDirection.ltr)
+      ..layout(minWidth: 0, maxWidth: double.infinity);
+    return textPainter.size;
+  }
 
   @override
   void initState() {
@@ -29,7 +38,72 @@ class _ShoppingBagState extends State<ShoppingBag> {
     bool showCartIcon  = false;
 
     return Scaffold(
+      backgroundColor: Colors.grey[200],
+      key: _scaffoldKey,
       appBar: header('Shopping Bag', _scaffoldKey, showCartIcon),
+      drawer: sidebar(context),
+      body: Container(
+        child: ListView.builder(
+          itemCount: bagItemList.length,
+          itemBuilder: (BuildContext context, int index){
+            var item = bagItemList[index];
+            return Padding(
+              padding: EdgeInsets.symmetric(vertical: 12.0,horizontal: 20.0),
+              child: Card(
+                elevation: 3,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0)
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      height: 230.0,
+                      width: 150.0,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                          image: DecorationImage(
+                              image: NetworkImage(
+                                  item['image'],
+                              ),
+                              fit: BoxFit.cover,
+                          )
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              item['name'],
+                              style: TextStyle(
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              "\$${item['price']}.00",
+                              style: TextStyle(
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.bold
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
