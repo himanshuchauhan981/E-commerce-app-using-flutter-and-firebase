fbe8bba0c6c24b7599cf2f5ccd37de3b6887f2dd
Himanshu Chauhan <himanshuchauhan981@gmail.com>
2020-05-08 20:03:22 +0530

    updated featured items view

--- app_frontend/lib/components/home/gridItemList.dart
+++ app_frontend/lib/components/home/gridItemList.dart
@@ -50,8 +50,13 @@ class _GridItemListState extends State<GridItemList> {
   }
 
   Widget build(BuildContext context){
+    var size = MediaQuery.of(context).size;
+
+    final double itemHeight = (size.height - kToolbarHeight - 24) / 2;
+    final double itemWidth = size.width / 2;
     return SliverGrid(
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          childAspectRatio: (itemWidth / itemHeight),
           crossAxisCount: 2,
           mainAxisSpacing: 20.0,
           crossAxisSpacing: 20.0,
@@ -68,41 +73,51 @@ class _GridItemListState extends State<GridItemList> {
 
   Widget featuredItemCard(item){
     return Card(
+      elevation: 0,
       semanticContainer: true,
       shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10.0)
+        borderRadius: BorderRadius.circular(10.0),
       ),
       clipBehavior: Clip.antiAlias,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Expanded(
-              child: Material(
-                child: InkWell(
-                  onTap: (){
-                    showParticularItem(item);
-                  },
-                  child: GridTile(
-                    footer: Container(
-                      color: Colors.white70,
-                      child: ListTile(
-                        leading: Text(
-                          item['name'],
-                          style: TextStyle(
-                              fontSize: 16.0
-                          ),
-                        ),
-                      ),
-                    ),
-                    child: FittedBox(
-                      child: Image.network(
-                        item['image'][0],
-                        fit: BoxFit.cover,
-                      ),
-                    ),
+            child: Material(
+              child: InkWell(
+                onTap: () {},
+                child: GridTile(
+                  child: Image.network(
+                    item['image'][0],
+                    fit: BoxFit.cover,
                   ),
                 ),
-              )
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "\$${item['price'].toString()}.00",
+                  style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+                Text(
+                  item['name'],
+                  overflow: TextOverflow.ellipsis,
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                      fontSize: 15.0,
+                      color: Colors.grey
+                  ),
+                ),
+              ],
+            ),
           )
         ],
       ),
