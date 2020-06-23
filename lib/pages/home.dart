6747f14027aa01df2ba38c34074f784e7f922fff
Himanshu Chauhan <himanshuchauhan981@gmail.com>
2020-06-23 16:00:31 +0530

    Fixes #41

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -17,55 +17,95 @@ class _HomeState extends State<Home> {
     final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
     bool showCartIcon  = true;
 
-    return Scaffold(
-        key: _scaffoldKey,
-        appBar: header('Shop Mart', _scaffoldKey,showCartIcon,context),
-        drawer: sidebar(context),
-        body: Container(
-          child: CustomScrollView(
-            slivers: <Widget>[
-              SliverList(
-                delegate: SliverChildListDelegate([
-                  Container(
-                      height: 80.0,
-                      child: CategoryCarousal()
-                  ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(0.0, 40.0, 0.0, 20.0),
-                    child: Center(
-                      child: Text(
-                        'New Arrivals',
-                        style: TextStyle(
-                            fontSize: 30.0,
-                            fontWeight: FontWeight.bold,
-                            letterSpacing: 1.2
-                        ),
-                      ),
-                    ),
-                  ),
-                  Container(
-                      height: 420.0,
-                      child: SnapEffectCarousel()
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 30.0,bottom: 15.0),
-                    child: Center(
-                      child: Text(
-                        'Featured',
-                        style: TextStyle(
-                            fontSize: 30.0,
-                            fontWeight: FontWeight.bold,
-                            letterSpacing: 1.2
-                        ),
-                      ),
-                    ),
-                  ),
-                ]),
+    return WillPopScope(
+      onWillPop: () async{
+        return(
+          await showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(24.0)
               ),
-              GridItemList(),
-            ],
-          ),
-        )
+              title: Text('Are you sure ?'),
+              content: Text('Do you want to exit an App'),
+              actions: <Widget>[
+                FlatButton(
+                  onPressed: () => Navigator.of(context).pop(false),
+                  child: Text(
+                    'No',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black
+                    )
+                  ),
+                ),
+                FlatButton(
+                  onPressed: () => Navigator.of(context).pop(true),
+                  child: Text(
+                    'Yes',
+                    style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.red
+                    )
+                  ),
+                )
+              ],
+            )
+          )
+        ) ?? false;
+      },
+      child: Scaffold(
+          key: _scaffoldKey,
+          appBar: header('Shop Mart', _scaffoldKey,showCartIcon,context),
+          drawer: sidebar(context),
+          body: Container(
+            child: CustomScrollView(
+              slivers: <Widget>[
+                SliverList(
+                  delegate: SliverChildListDelegate([
+                    Container(
+                        height: 80.0,
+                        child: CategoryCarousal()
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(0.0, 40.0, 0.0, 20.0),
+                      child: Center(
+                        child: Text(
+                          'New Arrivals',
+                          style: TextStyle(
+                              fontSize: 30.0,
+                              fontWeight: FontWeight.bold,
+                              letterSpacing: 1.2
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                        height: 420.0,
+                        child: SnapEffectCarousel()
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 30.0,bottom: 15.0),
+                      child: Center(
+                        child: Text(
+                          'Featured',
+                          style: TextStyle(
+                              fontSize: 30.0,
+                              fontWeight: FontWeight.bold,
+                              letterSpacing: 1.2
+                          ),
+                        ),
+                      ),
+                    ),
+                  ]),
+                ),
+                GridItemList(),
+              ],
+            ),
+          )
+      ),
     );
   }
 }
