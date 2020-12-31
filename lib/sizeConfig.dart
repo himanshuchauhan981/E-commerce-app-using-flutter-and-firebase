1aec74577217cb656aa1c26d8e8a07cf6dd1ed39
himanshuchauhan981 <himanshuchauhan981@gmail.com>
2020-12-31 20:56:00 +0530

    updated to responsive signup screen and tested with different screen size

---
+++ lib/sizeConfig.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/widgets.dart';
+
+class SizeConfig{
+  static MediaQueryData _mediaQueryData;
+  static double screenWidth;
+  static double screenHeight;
+  static double safeAreaHorizontal;
+  static double safeAreaVertical;
+  static double safeBlockHorizontal;
+  static double safeBlockVertical;
+  static String screenSize;
+
+  void init(BuildContext context){
+    _mediaQueryData = MediaQuery.of(context);
+    screenWidth = _mediaQueryData.size.width;
+    screenHeight = _mediaQueryData.size.height;
+    safeAreaHorizontal = _mediaQueryData.padding.left + _mediaQueryData.padding.right;
+    safeAreaVertical = _mediaQueryData.padding.top + _mediaQueryData.padding.bottom;
+
+    safeBlockHorizontal = (screenWidth - safeAreaHorizontal) / 100;
+    safeBlockVertical = (screenHeight - safeAreaVertical) / 100;
+
+    if(screenWidth >= 400){
+      screenSize = 'largeMobile';
+    }
+    else if(screenWidth >= 600){
+      screenSize = 'tablet';
+    }
+    else{
+      screenSize = 'smallMobile';
+    }
+
+  }
+
+}
